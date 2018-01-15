9fd785b826ab3fb9bb1648a45592300ea9fb6f68
sasakin <max_to_day@mail.ru>
2018-01-15 15:03:42 +0300

    release 1.0

---
+++ src/main/java/MassangerProducer.java
@@ -0,0 +1,26 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class MassangerProducer {
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setVirtualHost("/");
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+        String exchangeName = "myExchange";
+        String routingKey = "testRoute";
+
+        for (int i=1; i<21; i++) {
+            byte[] messageBodyBytes = ("test "+i).getBytes();
+            channel.basicPublish(exchangeName, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, messageBodyBytes);
+        }
+        channel.close();
+        conn.close();
+    }
+}
